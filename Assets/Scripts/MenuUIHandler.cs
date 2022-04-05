0c040396c8e6f3850b43601e0b003662e55c46fa
inuksuktv <75352411+inuksuktv@users.noreply.github.com>
2022-04-05 14:29:03 -0600

    Input Field
    Read and store input

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 // Sets the script to be executed later than all default scripts
 [DefaultExecutionOrder(1000)]
 public class MenuUIHandler : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     public void StartNew()
